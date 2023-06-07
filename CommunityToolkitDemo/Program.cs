e313e45fb65c30cfb1490777f3b3b9d047fedaf2
thecsharper <si@thecsharper.com>
2023-06-07 15:17:54 +0100

    Add project files.

---
+++ CommunityToolkitDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/communitytoolkit/high-performance/introduction
+
+using CommunityToolkit.HighPerformance.Helpers;
+
+var array = Enumerable.Range(1, 100).Select(x => x * x).ToArray();
+
+ParallelHelper.For(0, array.Length, new ArrayInitializer(array));
+
+readonly struct ArrayInitializer : IAction
+{
+    private readonly int[] _array;
+
+    public ArrayInitializer(int[] array)
+    {
+        _array = array;
+    }
+
+    public void Invoke(int i)
+    {
+        _array[i] = i;
+        Console.WriteLine(i);
+    }
+}
+
